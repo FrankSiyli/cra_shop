8c1e10a50966405183f26309ea365ed6a72437df
imac <frank.siyli@gmail.com>
2023-05-09 12:39:30 +0200

    all products page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const Product = () => {
   const [products, getProducts] = useState([]);
@@ -8,10 +10,32 @@ const Product = () => {
       .then((json) => getProducts(json));
   }, []);
 
+  const cards = products.map((product) => (
+    <div className="col-md-3" style={{ marginBottom: "10px" }}>
+      <Card key={product.id} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: "100px", height: "auto" }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>{product.description.substring(0, 40)}...</Card.Text>
+          <Card.Text>{product.price}€</Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <Button variant="primary">Add to cart</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
+
   return (
     <>
       <h1>Product</h1>
-      {JSON.stringify(products)}
+      <div className="row">{cards}</div>
     </>
   );
 };
